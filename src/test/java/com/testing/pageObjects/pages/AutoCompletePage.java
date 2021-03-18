d1513930d021943e5c7bf6c514a6100ab6f68ae2
miks45 <80906897+miks45@users.noreply.github.com>
2021-03-18 17:06:13 +0200

    Add files via upload

--- src/test/java/com/testing/pageObjects/pages/AutoCompletePage.java
+++ src/test/java/com/testing/pageObjects/pages/AutoCompletePage.java
@@ -10,11 +10,11 @@ import net.thucydides.core.annotations.DefaultUrl;
 @DefaultUrl("/auto-complete")
 public class AutoCompletePage extends WidgetsPage{
   // Locators ----------------------------------------------------------------------------------------------------------
-  public static By AUTO_COMPLETE_TITLE;
+  public static By AUTO_COMPLETE_TITLE = text("Auto Complete");
   // Element to which you would send keystrokes
-  public static By MULTIPLE_COLOR_FIELD;
+  public static By MULTIPLE_COLOR_FIELD = css("input[id='auto-complete__value-container auto-complete__value-container--is-multi css-1hwfws3']") ;
   // Element containing the information about actual values
-  public static By MULTIPLE_COLOR_CONTAINER;
+  public static By MULTIPLE_COLOR_CONTAINER = css("input[id='autoCompleteMultiInput']");
   // Element to which you would send keystrokes
   public static By SINGLE_COLOR_FIELD;
   // Element containing the information about actual values
@@ -29,6 +29,9 @@ public class AutoCompletePage extends WidgetsPage{
     switch(elementName){
       case "SINGLE_COLOR_FIELD":
       case "MULTIPLE_COLOR_FIELD":
+        getElement(elementName).click();
+        getElement(elementName).sendKeys(value);
+        getElement(elementName).sendKeys(Keys.ENTER);
         // Create a logic that would input the value into given fields, and press (or send) the enter key (Keys.ENTER)
         WebElementFacade e = getElement(elementName);
         
